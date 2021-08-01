ec8dc7c75837deac0fd8f7cc9f23b542a1676f76
Al Bluemle <abluemleiv@gmail.com>
2021-08-01 10:36:32 -0400

    Creates changes and removed code for HPool

--- miner_monitoring.py
+++ miner_monitoring.py
@@ -14,43 +14,28 @@ current_path = os.path.dirname(__file__)
 
 
 def check_miners():
-    capacity = check_miner(miner_settings['hpool_path'])
-    plot_num = int(float(capacity.strip(" TB"))//0.09898437)
     check_chia = process_running('Chia')
-    check_hpool = process_running('hpool-miner-chia-gui.exe')
     chia_balance = check_balance(miner_settings['chia_path'])
     previous_balance = old_balance(current_path)
     farm = check_farm(miner_settings['chia_path'])
-    farm_size = float(farm[1]) + float(capacity)
+    farm_size = float(farm[1])
 
 
-    if miner_settings['always_notify'] and check_chia and check_hpool:
+    if miner_settings['always_notify'] and check_chia:
         send_notifications(notifications['miner_webhook_true'],
                     'LOOKS GOOD!',
-                    'Miners are running.',
+                    'Miner is running.',
                     '3aab59',
-                    [['__**HPOOL MINER RUNNING:**__', f'''Using about: {plot_num} plots.
-Mining capacity: {capacity} TiB'''], ['__**CHIA BLOCKCHAIN RUNNING:**__', f'''{farm[0]}
-Size of plots: {farm[1]} TiB'''], ['__**TOTALS:**__', f'''Farm capacity: {farm_size:.2f} TiB
+                    [['__**CHIA BLOCKCHAIN RUNNING:**__', f'''{farm[0]}'''], ['__**TOTALS:**__', f'''Farm capacity: {farm_size:.2f} TiB
 {'NEW COIN IN WALLET!!! Balance = ' if compare_chia(chia_balance, previous_balance) else 'Chia Balance = '}{chia_balance} xch''']])
-    elif check_chia and not check_hpool:
-        send_notifications(notifications['miner_webhook_false'],
-                    '**HPOOL MINER IS NOT RUNNING.**',
-                    'Restart the HPool process to resume mining.The Chia Blockchain is still online.',
-                    '9f0100',
-                    [])
-    elif not check_chia and check_hpool:
+    elif not check_chia:
         send_notifications(notifications['miner_webhook_false'],
                     '**CHIA BLOCKCHAIN IS NOT RUNNING.**',
                     'Restart the Chia process to resume mining.The HPOOL miner is still online.',
                     '9f0100',
                     [])
     else:
-        send_notifications(notifications['miner_webhook_false'],
-                    '**MINERS ARE NOT RUNNING!**',
-                    'Check the mining computer. Restart both miners to resume collecting coin.',
-                    '9f0100',
-                    [])
+        pass
 
     update_log(current_path, chia_balance)
 
